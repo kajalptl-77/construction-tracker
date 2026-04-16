bc0f1b7c25c167a0c481aacc278eb7f3417ffe0f
kajalptl-77 <kajalptl77@gmail.com>
2026-04-17 00:54:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,19 @@ app.use(express.json());
 
 // Database connection pool
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'Password@0157',
-    database: process.env.DB_NAME || 'construction_tracker',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // JWT Secret
